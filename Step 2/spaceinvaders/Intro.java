29f39c93c3ae08552b602ae10559bdd42c8526c0
Gallosimone23 <56158532+Gallosimone23@users.noreply.github.com>
2020-04-02 15:29:40 +0200

    Add files via upload

---
+++ Step 2/spaceinvaders/Intro.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author Roberto
+ */
+public class Intro extends Spazio {
+    private int i = 0;
+    
+    void run() throws InterruptedException {
+       setBackground (Color.BLACK);
+       setSize(800, 600);
+               
+        for(i=100;i>=0;i-=1) {
+            System.out.println("i: " + i);
+            this.repaint();
+            Thread.sleep(50);
+        }
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.setFont(new Font("Bold", Font.PLAIN, 40+i));
+        g.drawString("Space Invaders", 100+i, 100+i);
+        g.setFont(new Font("Bold", Font.PLAIN, 10+i));
+        g.drawString("          by Simone Gallo", 110+i+i, 110+i+i);
+        
+        
+    }
+
+}
